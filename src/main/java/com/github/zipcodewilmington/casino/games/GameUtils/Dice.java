cb38ca0731fa31a0bc1fa48a74599a042b65fc37
hyunjialee <hyjialee@gmail.com>
2023-11-13 23:59:52 -0500

    fixed dice

--- src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/Dice.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/Dice.java
@@ -7,73 +7,61 @@ public class Dice {
     private int value1;
     private int value2;
     private Random rand;
-    private static final String[][] diceFace = {
-            {
-                    "+-------+",
-                    "|       |",
-                    "|   ●   |",
-                    "|       |",
-                    "+-------+",
-            }, {
-                    "+-------+",
-                    "| ●     |",
-                    "|       |",
-                    "|     ● |",
-                    "+-------+",
-            }, {
-                    "+-------+",
-                    "| ●     |",
-                    "|   ●   |",
-                    "|     ● |",
-                    "+-------+",
-            }, {
-                    "+-------+",
-                    "| ●   ● |",
-                    "|       |",
-                    "| ●   ● |",
-                    "+-------+",
-            }, {
-                    "+-------+",
-                    "| ●   ● |",
-                    "|   ●   |",
-                    "| ●   ● |",
-                    "+-------+",
-            }, {
-                    "+-------+",
-                    "| ●   ● |",
-                    "| ●   ● |",
-                    "| ●   ● |",
-                    "+-------+",
-            }
-};
-    private String [] face1;
-    private String [] face2;
+//    private static final String[][] diceFace = {
+//            {
+//                    "+-------+",
+//                    "|       |",
+//                    "|   ●   |",
+//                    "|       |",
+//                    "+-------+",
+//            }, {
+//                    "+-------+",
+//                    "| ●     |",
+//                    "|       |",
+//                    "|     ● |",
+//                    "+-------+",
+//            }, {
+//                    "+-------+",
+//                    "| ●     |",
+//                    "|   ●   |",
+//                    "|     ● |",
+//                    "+-------+",
+//            }, {
+//                    "+-------+",
+//                    "| ●   ● |",
+//                    "|       |",
+//                    "| ●   ● |",
+//                    "+-------+",
+//            }, {
+//                    "+-------+",
+//                    "| ●   ● |",
+//                    "|   ●   |",
+//                    "| ●   ● |",
+//                    "+-------+",
+//            }, {
+//                    "+-------+",
+//                    "| ●   ● |",
+//                    "| ●   ● |",
+//                    "| ●   ● |",
+//                    "+-------+",
+//            }
+//};
+
     public Dice(){
         this.rand = new Random();
         this.value1 = 0;
         this.value2 = 0;
-        this.face1 = diceFace[0];
-        this.face2 = diceFace[0];
     }
 
     public int rollDice(){
         int value1 = rand.nextInt(6) + 1;
-        generateDice(value1);
         return value1;
     }
-    public int rollTwoDice(){
+    public int rollTwoDice() {
         int value1 = rand.nextInt(6) + 1;
         int value2 = rand.nextInt(6) + 1;
-        generateDice(value1);
-        generateDice(value2);
         return value1 + value2;
     }
-
-    public void generateDice (int value){
-        face1 = diceFace[value - 1];
-        face2 = diceFace[value - 1];
-    }
-
     public int getValue(){
         return this.value1;
     }
