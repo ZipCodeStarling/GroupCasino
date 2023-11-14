2ed5e90fec91947eae61ba2df6fd5c10df586ee6
hyunjialee <hyjialee@gmail.com>
2023-11-13 23:53:36 -0500

    add dice class

--- src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/Dice.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/Dice.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Dice {
 
-    private int value;
+    private int value1;
+    private int value2;
     private Random rand;
-
     private static final String[][] diceFace = {
             {
                     "+-------+",
@@ -46,25 +46,38 @@ public class Dice {
                     "+-------+",
             }
 };
-    private String [] face;
+    private String [] face1;
+    private String [] face2;
     public Dice(){
         this.rand = new Random();
+        this.value1 = 0;
+        this.value2 = 0;
+        this.face1 = diceFace[0];
+        this.face2 = diceFace[0];
     }
 
     public int rollDice(){
-        int value = rand.nextInt(6) + 1;
-        generateDice(value);
-        return getValue();
+        int value1 = rand.nextInt(6) + 1;
+        generateDice(value1);
+        return value1;
+    }
+    public int rollTwoDice(){
+        int value1 = rand.nextInt(6) + 1;
+        int value2 = rand.nextInt(6) + 1;
+        generateDice(value1);
+        generateDice(value2);
+        return value1 + value2;
     }
 
     public void generateDice (int value){
-        this.face = this.diceFace[value - 1];
-    }
-    public String[] getDice(){
-        return this.face;
+        face1 = diceFace[value - 1];
+        face2 = diceFace[value - 1];
     }
 
     public int getValue(){
-        return this.value;
+        return this.value1;
+    }
+    public int getValue2(){
+        return this.value2;
     }
 }
